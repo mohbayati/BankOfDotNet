459f5208c6b773fea537e27ce8ba058218c43cf3
mohbayati <m.bayati14@gmail.com>
2021-11-12 23:08:38 +0330

    Add project files.

---
+++ BankOfDotNet.API/Data/BankOfDotNetAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BankOfDotNet.API.Models;
+
+namespace BankOfDotNet.API.Data
+{
+    public class BankOfDotNetAPIContext : DbContext
+    {
+        public BankOfDotNetAPIContext (DbContextOptions<BankOfDotNetAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BankOfDotNet.API.Models.Customer> Customer { get; set; }
+    }
+}
