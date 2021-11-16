825b3e791602d28ed4e035cb7c594304e990e25e
mohbayati <m.bayati14@gmail.com>
2021-11-16 06:24:23 +0330

    console

---
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            //discover all endpoint using metadata of identity server
+            var client = new HttpClient();
+
+            //var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5000");
+            //if (disco.IsError) throw new Exception(disco.Error);
+
+            //var tokenClient = new TokenClient(disco.TokenEndpoint, new TokenClientOptions { ClientId="client",ClientSecret="secret"})
+
+            var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = "http://localhost:5000/connect/token",
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "bankOfDotNetApi"
+            });
+
+            Console.WriteLine(response.AccessToken);
+            Console.ReadLine();
+        }
+    }
+}
